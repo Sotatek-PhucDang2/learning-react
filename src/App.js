4b00965f73cbd4b24adeb30185bd509fa0c66ea1
ngoc phuc <ngocphuc1607@gmail.com>
2024-12-12 15:39:05 +0700

    chuyen sang react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './component/Header';
+import Button from './component/Button';
+import Display from './component/Display';
+import "./App.css"
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Display/>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Display.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Display.css"
+const Display=()=>{
+return(
+    <div class="display">0</div>
+)
+}
+export default Display;
