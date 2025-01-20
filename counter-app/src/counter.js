4527b117f4866866d087983745201502048f2cb9
Sotatek-PhucDang2 <phuc.dang2@sotatek.com>
2025-01-20 11:17:54 +0700

    Update counter.js

--- counter-app/src/counter.js
+++ counter-app/src/counter.js
@@ -1,17 +1,49 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 
-function Counter (){
-    const[count , setCount] =useState(0);
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState("");
 
-    return (
-        <div className="container">
-            <h1>Số lần:  {count} </h1>
-            <button onClick = {()=> setCount(count + 1)}>+</button>
-            <button onClick = {()=> setCount(count - 1 )}>-</button>
-            <button onClick = {()=> setCount(0)}>reset</button>
-        </div>
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setStep(value); 
+    if (value && isNaN(value)) {
+        alert("Vui lòng nhập số");
+      }
+  };
 
-    );
+  const handleIncrease = () => {
+    const validStep = isNaN(step) || step === '' ? 1 : parseInt(step);
+    setCount(count + validStep);
+  };
 
+  const handleDecrease = () => {
+    const validStep = isNaN(step) || step === '' ? 1 : parseInt(step);
+    setCount(count - validStep);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setStep(""); 
+  };
+
+  return (
+    <div className='container'>
+      <h1>Số lần click là: {count}</h1>
+
+      <h2>Số bước nhảy</h2>
+      <input
+        type="text"
+        value={step}
+        onChange={handleChange}
+        placeholder="Nhập số phần tử muốn tăng hoặc giảm"
+      />
+
+      <button onClick={handleIncrease}>+</button>
+      <button onClick={handleDecrease}>-</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
 }
+
 export default Counter;
